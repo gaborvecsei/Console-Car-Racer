b2400bdeaf71fff3d84cc38dc749827be13d7604
Gabor Vecsei <vecseigabor.x@gmail.com>
2016-12-04 23:18:42 +0100

    Score table

--- car_racing.py
+++ car_racing.py
@@ -12,13 +12,26 @@
 
 # Windows OS specific:
 import msvcrt
-import sys
+import os
 import time
+from datetime import datetime
 
 import numpy as np
+import pandas as pd
 
 from Cars.cars import EnemyCar, Player
 from Court.court import Court
+
+COURT_WIDTH = 20
+COURT_HEIGHT = 10
+PLAYER_Y_POS = COURT_HEIGHT - 2
+ENEMY_START_Y_POS = 0 + 2
+
+SCORES_PATH = "data/scores.csv"
+
+
+def cleanScreen():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 
 # Asks whether a key has been acquired
@@ -38,68 +51,97 @@ def createNewEnemy(minSpeed, maxSpeed, court):
     return newEnemy
 
 
-COURT_WIDTH = 20
-COURT_HEIGHT = 10
-PLAYER_Y_POS = COURT_HEIGHT - 2
-ENEMY_START_Y_POS = 0 + 2
+def main(playerName, highScore = 0):
+    court = Court(COURT_WIDTH, COURT_HEIGHT)
 
-court = Court(COURT_WIDTH, COURT_HEIGHT)
+    player = Player()
+    rndXPosPlayer = court.generateRandomXPos()
+    player.setPosition((rndXPosPlayer, PLAYER_Y_POS))
 
-player = Player()
-rndXPosPlayer = court.generateRandomXPos()
-player.setPosition((rndXPosPlayer, PLAYER_Y_POS))
+    enemy1 = createNewEnemy(5, 10, court)
+    enemy2 = createNewEnemy(5, 10, court)
 
-enemy1 = createNewEnemy(5, 10, court)
-enemy2 = createNewEnemy(5, 10, court)
+    enemies = [enemy1, enemy2]
 
-ENEMIES = [enemy1, enemy2]
+    time_delta = 1. / 33
 
-time_delta = 1. / 33
+    while True:
+        time.sleep(time_delta)
+        # Detect user input
+        keyPressed = userKeyboardInput()
+        if keyPressed is not None:
+            player.controllPlayer(keyPressed)
 
-while True:
-    time.sleep(time_delta)
-    # Detect user input
-    keyPressed = userKeyboardInput()
-    if keyPressed is not None:
-        player.controllPlayer(keyPressed)
+        for enemy in enemies:
+            enemy.moveOneStep()
+            if enemy.detectWallCollision(court):
+                enemies.remove(enemy)
 
-    for enemy in ENEMIES:
-        enemy.moveOneStep()
-        if enemy.detectWallCollision(court):
-            ENEMIES.remove(enemy)
+                player.increaseScore()
 
-            player.increaseScore()
+                playerScore = player.getScore()
 
-            playerScore = player.getScore()
+                # Increase speed of the newborn enemies
+                if playerScore < 10:
+                    newEnemy = createNewEnemy(2, 10, court)
+                elif 10 < playerScore < 20:
+                    newEnemy = createNewEnemy(5, 15, court)
+                elif 20 < playerScore < 30:
+                    newEnemy = createNewEnemy(10, 20, court)
+                else:
+                    newEnemy = createNewEnemy(15, 25, court)
+                enemies.append(newEnemy)
 
-            # Increase speed of the newborn enemies
-            if playerScore < 10:
-                newEnemy = createNewEnemy(2, 10, court)
-            elif 10 < playerScore < 20:
-                newEnemy = createNewEnemy(5, 15, court)
-            elif 20 < playerScore < 30:
-                newEnemy = createNewEnemy(10, 20, court)
-            else:
-                newEnemy = createNewEnemy(15, 25, court)
-            ENEMIES.append(newEnemy)
+                # Increase the number of enemies
+                if player.getScore() % 10 == 0:
+                    newEnemy = createNewEnemy(5, 20, court)
+                    enemies.append(newEnemy)
 
-            # Increase the number of enemies
-            if player.getScore() % 10 == 0:
-                newEnemy = createNewEnemy(5, 20, court)
-                ENEMIES.append(newEnemy)
+        # Detect if the player collided with an enemy
+        if player.detectCarCollision(enemies) or player.detectWallCollision(court):
+            print "\nCollision!"
+            break
 
-    # Detect if the player collided with an enemy
-    if player.detectCarCollision(ENEMIES):
-        print "\nCar Collision!"
-        sys.exit(0)
+        # Draw the players and enemies on the court
+        court.putPlayersOnCourt([player])
+        court.putEnemiesOnCourt(enemies)
+        # Show the court in the console
+        court.printCourt()
+        print "\nPlayerScore: {0}".format(player.getScore())
 
-    if player.detectWallCollision(court):
-        print "\nWall Collision!"
-        sys.exit(0)
+    newRow = ','.join([playerName, str(player.getScore()), str(datetime.now())])
+    print newRow
+    with open(SCORES_PATH, "a") as f:
+        f.write("\n" + newRow)
 
-    # Draw the players and enemies on the court
-    court.putPlayersOnCourt([player])
-    court.putEnemiesOnCourt(ENEMIES)
-    # Show the court in the console
-    court.printCourt()
-    print "\nPlayerScore: {0}".format(player.getScore())
+    if player.getScore() > highScore:
+        print "You beat the high score!"
+
+
+def showHighScore():
+    scoresTable = pd.read_csv(SCORES_PATH, header=0)
+
+    names = scoresTable["name"].values
+    scores = scoresTable["score"].values
+    times = scoresTable["time"].values
+
+    if len(names) > 0:
+
+        highScoreRowIndex = np.argmax(scores)
+
+        highScore = scores[highScoreRowIndex]
+        highScoreName = names[highScoreRowIndex]
+        highScoreTime = times[highScoreRowIndex]
+
+        print "High score reached by {0}:\n\tscore: {1}\n\tdate: {2}".format(highScoreName, highScore, highScoreTime)
+        return highScore
+    else:
+        return 0
+
+
+if __name__ == '__main__':
+    print "Car racer by Gabor Vecsei\n"
+    highScore = showHighScore()
+    playerName = raw_input("Enter your name: ")
+    cleanScreen()
+    main(playerName, highScore)
