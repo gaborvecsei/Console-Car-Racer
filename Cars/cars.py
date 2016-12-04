c96e9d5de782672ee5388f186e68625d7f6279ba
Gabor Vecsei <vecseigabor.x@gmail.com>
2016-12-04 18:35:53 +0100

    new classes

---
+++ Cars/cars.py
@@ -0,0 +1,98 @@
+class Car(object):
+    def __init__(self, startPosition=(0, 0), character='#', direction=0):
+        # Character that represents the car in the court
+        self.character = character
+        # The direction where it is going (left = 0, right = 1)
+        self.direction = direction
+        self.position = startPosition
+
+    def changeDirection(self, dir):
+        self.direction = dir
+
+    def getDirection(self):
+        return self.direction
+
+    def getPosition(self):
+        return self.position
+
+    def getCharacter(self):
+        return self.character
+
+    def setPosition(self, position):
+        self.position = position
+
+
+class Player(Car):
+    def __init__(self):
+        super(self.__class__, self).__init__()
+        self.score = 0
+        self.character = "O"
+
+    def increaseScore(self):
+        self.score += 1
+
+    def getScore(self):
+        return self.score
+
+    # Changes the position of the Player based on the direction
+    def move(self):
+        # Left
+        if self.getDirection() == 0:
+            self.setPosition((self.position[0] - 1, self.position[1]))
+        # Right
+        elif self.getDirection() == 1:
+            self.setPosition((self.position[0] + 1, self.position[1]))
+
+    def controllPlayer(self, keyboardInput):
+        # Left
+        if keyboardInput.decode() == 'a':
+            self.changeDirection(0)
+            self.move()
+        # Right
+        elif keyboardInput.decode() == 'd':
+            self.changeDirection(1)
+            self.move()
+
+    def detectCarCollision(self, enemies):
+        for enemy in enemies:
+            if enemy.getPosition() == self.getPosition():
+                return True
+        return False
+
+    def detectWallCollision(self, court):
+        x,y = self.position
+        if x >= court.getWidthHeight()[0]-1:
+            return True
+        elif x < 1:
+            return True
+        elif y >= court.getWidthHeight()[1]-1:
+            return True
+        elif y < 1:
+            return True
+        else:
+            return False
+
+
+class EnemyCar(Car):
+    def __init__(self, speed):
+        super(self.__class__, self).__init__()
+        self.speed = speed
+        self.character = "X"
+        # We use this to determine the "speed" of the enemy car
+        self.timeCount = 0
+
+    # speed: the larger the number the slower the car
+    def moveOneStep(self):
+        if self.timeCount > self.speed:
+            self.setPosition((self.position[0], self.position[1] + 1))
+            self.timeCount = 0
+        else:
+            self.timeCount += 1
+
+    def detectWallCollision(self, court):
+        if self.position[0] >= court.getWidthHeight()[0]:
+            return True
+        elif self.position[1] >= court.getWidthHeight()[1]:
+            return True
+        else:
+            return False
---
+++ Court/court.py
@@ -0,0 +1,72 @@
+import sys
+
+import numpy as np
+
+
+class Court():
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.court = []
+        self.buildCourt()
+
+    def buildCourt(self):
+        # Build the court
+        for i in range(self.height):
+            court_row = []
+            for k in range(self.width):
+                if i == 0:
+                    court_row.append('-')
+                elif i == self.height - 1:
+                    court_row.append('-')
+                elif k == 0 and i != 0:
+                    court_row.append('|')
+                elif k == self.width - 1:
+                    court_row.append('|')
+                else:
+                    court_row.append(' ')
+            if len(court_row) > 0:
+                self.court.append(court_row)
+
+    def printCourt(self):
+        # Prepare a string that represents the court
+        screen = "\n".join(''.join(line) for line in self.court)
+        # Print the court
+        sys.stdout.write(screen)
+        sys.stdout.flush()
+        self.clearCourt()
+
+    def putOnCourt(self, position, char):
+        x, y = position
+        self.court[y][x] = char
+
+    def clearCourt(self):
+        self.court = []
+        self.buildCourt()
+
+    def getCourt(self):
+        return self.court
+
+    def getWidthHeight(self):
+        return self.width, self.height
+
+    def putPlayersOnCourt(self, players):
+        # i is the index in the players array
+        for i, player in enumerate(players):
+            self.putOnCourt(player.getPosition(), player.getCharacter())
+
+    def putEnemiesOnCourt(self, enemies):
+        # i is the index in the players array
+        for i, enemy in enumerate(enemies):
+            self.putOnCourt(enemy.getPosition(), enemy.getCharacter())
+
+    # Generates a random position on the court
+    # We can define a box for the generation (Xmin, Xmax, Ymin, Ymax)
+    def generateRndPosOnCourt(self, box=None):
+        if box is None:
+            box = (1, self.width - 2, 1, self.height - 2)
+        xMin, xMax, yMin, yMax = box
+        return np.random.randint(xMin, xMax), np.random.randint(yMin, yMax)
+
+    def generateRandomXPos(self):
+        return np.random.randint(1, self.width - 2)
